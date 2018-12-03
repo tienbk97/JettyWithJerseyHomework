0bbd4f913734afd49ac88930430343fd39d24a12
Tien Nguyen <nghuuanhtien@gmail.com>
2018-12-03 17:54:06 +0700

    Edit limit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,8 +18,8 @@ public class App {
         context.addFilter( LogFilter.class, "/*", REQUEST_SCOPE );
         // For limit
         FilterHolder filterHolder = new FilterHolder( DoSFilter.class );
-        filterHolder.setInitParameter("maxRequestsPerSec", "1");  // max requests per second per client
-        filterHolder.setInitParameter("delayMs", "-1");           // millisec to delay excess requests. -1 means reject (for testing)
+        filterHolder.setInitParameter("maxRequestsPerSec", "10");  // max requests per second per client
+        filterHolder.setInitParameter("delayMs", "1000");           // millisec to delay excess requests. -1 means reject (for testing)
         filterHolder.setInitParameter("remotePort", "false");     // true = track connections by remote ip+port
         filterHolder.setInitParameter("enabled", "true");
         filterHolder.setInitParameter("trackSessions", "true");
